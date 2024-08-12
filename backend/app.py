89024e23fddbe2c2b1ad6418578ce2a0e2e0c8f2
Ayomide Aremu-Cole <121998651+ayomidecole@users.noreply.github.com>
2024-08-12 16:17:44 +0000

    Bug fixes, fixed database configuration in app.py.  Changed postgres://neondb_owner to postgresql://neondb_owner

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,14 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://neondb_owner:7NVh5GIousQl@ep-sweet-wave-a5ydk6cx.us-east-2.aws.neon.tech/neondb?sslmode=require&options=project%3Dep-sweet-wave-a5ydk6cx'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 import routes
+
+with app.app_context():
+  db.create_all
 
 if __name__ == "__main__":
   app.run(debug=True)
